f79eaa48a31b9e34bc2107b09dd31ab106bbf464
mattlag <mattlag@gmail.com>
2016-01-15 18:37:22 -0800

    Appears to be working!

--- app/script.js
+++ app/script.js
@@ -1,59 +1,32 @@
-// start of file
-/**
-	Page > Test Drive
-	HTML and associated functions for this page.
-**/
 
-
-	/*
-		loadPage_testdrive
-		This function is called by the overall Navigate function, and populates
-		page-level HTML.
-	*/
-	function loadPage_testdrive(){
-		// debug("LOADING PAGE >> loadPage_testdrive");
-
-		update_NavPanels();
-
-		var content = '<h1 class="pagetitle">Test Drive</h1><div class="pagecontent">' +
-			'<textarea id="tdtextarea" onkeyup="_UI.testdrive.sampletext=this.value; redraw_TestDrive()">'+_UI.testdrive.sampletext+'</textarea><br>' +
-			'<canvas id="tdcanvas"></canvas><br>' +
-			'<div id="genimg" style="display:none;"></div></div>';
-
-		getEditDocument().getElementById("mainwrapper").innerHTML = content;
-		document.getElementById("tdtextarea").focus();
-
+	function coreMode_OnLoad(){
+ 		debug('\n coreMode_OnLoad - START');
+ 
 		_UI.testdrive.canvas = document.getElementById('tdcanvas');
 		_UI.testdrive.canvas.width = 800;
 		_UI.testdrive.canvas.height = 700;
 		_UI.testdrive.ctx = _UI.testdrive.canvas.getContext('2d');
-
 		_UI.testdrive.cache = {};
 
+		debug('\t loading font');
+
+		hydrateGlyphrProject(_UI.font);
+
+		// var f = evt.dataTransfer || document.getElementById('filechooser');
+		// var reader = new FileReader();
+
+		// reader.onload = function() {
+		// 	importGlyphrProjectFromText(reader.result);
+		// 	redraw_TestDrive();
+		// };
+
+		// reader.readAsText(f);
+
+		debug('\t redrawing');
 		redraw_TestDrive();
+
+		debug(' coreMode_OnLoad - END\n');
 	}
-
-	/*
-		makePanel_TestDriveAttributes
-		This function is called by the overall Redraw function, and it loads content to
-		the left panel - usually options for the Edit Canvas.
-	*/
-	function makePanel_TestDriveAttributes(){
-		if(_UI.navprimaryhere !== 'npAttributes') return;
-
-		var content = '<div class="navarea_header">';
-		content += '<h1 class="panelsupertitle">TEST DRIVE</h1>';
-		content += '<h1 class="paneltitle">controls</h1>';
-
-		content += '</div><div class="panel_section">';
-		content += '<h2>options</h2><div>' + drawTDOptions() + '</div>';
-		content += '</div><div class="panel_section">';
-		content += '<h2>sample text</h2><div>' + drawSampletextButtons() + '</div>';
-		content += '</td></tr></table></div>';
-
-		return content;
-	}
-
 
 	/*
 		readraw_TestDrive
@@ -61,14 +34,18 @@
 		content is in the main Content Area
 	*/
 	function redraw_TestDrive(){
-		// debug("\n redraw_TestDrive - START");
+		debug("\n redraw_TestDrive - START");
 		_UI.redrawing = true;
 
 		var td = _UI.testdrive;
 		var ps = _GP.projectsettings;
+		var txtarea = document.getElementById('tdtextarea');
 
-		if(_UI.navprimaryhere === 'npAttributes') changefontscale(td.fontsize);
+		_UI.testdrive.sampletext = txtarea.value; 
+
 		document.getElementById('tdtextarea').value = td.sampletext;
+		document.getElementById('tdoptions').innerHTML = drawTDOptions();
+		changefontscale(td.fontsize);
 
 		var contentarray = td.sampletext.split('');
 		contentarray = findAndMergeLigatures(contentarray);
@@ -83,7 +60,7 @@
 		tctx.clearRect(0,0,5000,5000);
 		if(td.showhorizontals) drawLine(curry);
 
-		// debug('\t contentarray.length: ' + contentarray.length);
+		debug('\t contentarray.length: ' + contentarray.length);
 
 		for(var k=0; k<contentarray.length; k++){
 			tc = contentarray[k];
@@ -114,8 +91,8 @@
 						);
 					}
 
-					// debug('\t starting drawing ' + cc.name);
-					// debug(cc);
+					debug('\t starting drawing ' + cc.name);
+					debug(cc);
 
 					if(_UI.testdrive.flattenglyphs){
 						if(!_UI.testdrive.cache.hasOwnProperty(tc)){
@@ -130,7 +107,7 @@
 
 					currx += (td.padsize*1*scale);
 					currx += calculateKernOffset(tc, contentarray[k+1])*scale;
-					// debug('\t done drawing ' + cc.name);
+					debug('\t done drawing ' + cc.name);
 				}
 			}
 		}
@@ -146,14 +123,14 @@
 		are not returned, only kern values.
 	*/
 	function calculateKernOffset(c1, c2) {
-		// debug('\n calculateKernOffset - START');
-		// debug('\t passed: ' + c1 + ' and ' + c2);
+		debug('\n calculateKernOffset - START');
+		debug('\t passed: ' + c1 + ' and ' + c2);
 
 		if(!c1 || !c2) return 0;
 
 		c1 = parseUnicodeInput(c1).join('');
 		c2 = parseUnicodeInput(c2).join('');
-		// debug('\t converted: ' + c1 + ' and ' + c2);
+		debug('\t converted: ' + c1 + ' and ' + c2);
 
 		var k = _GP.kerning;
 		var tlc, trc, re;
@@ -161,14 +138,14 @@
 		for(var p in k){ if(k.hasOwnProperty(p)){
 			for(var l=0; l<k[p].leftgroup.length; l++){
 				tlc = k[p].leftgroup[l];
-				// debug('\t checking leftgroup ' + tlc + ' against ' + c1);
+				debug('\t checking leftgroup ' + tlc + ' against ' + c1);
 				if(parseUnicodeInput(tlc)[0] === c1){
-					// debug('\t LEFTGROUP MATCH! for ' + c1);
+					debug('\t LEFTGROUP MATCH! for ' + c1);
 					for(var r=0; r<k[p].rightgroup.length; r++){
 						trc = k[p].rightgroup[r];
 						if(parseUnicodeInput(trc)[0] === c2){
 							re = (k[p].value*-1);
-							// debug('\t FOUND MATCH! returning ' + re);
+							debug('\t FOUND MATCH! returning ' + re);
 							return re;
 						}
 					}
@@ -176,7 +153,7 @@
 			}
 		}}
 
-		// debug(' calculateKernOffset - END\n');
+		debug(' calculateKernOffset - END\n');
 		return 0;
 	}
 
@@ -186,32 +163,32 @@
 		that merge to ligatures.  Returns an array with merged results.
 	*/
 	function findAndMergeLigatures(carr) {
-		// debug('\n findAndMergeLigatures - START');
+		debug('\n findAndMergeLigatures - START');
 		var ligs = sortLigatures();
-		// debug('\t sorted ligs: ');
-		// debug(ligs);
+		debug('\t sorted ligs: ');
+		debug(ligs);
 
 		var ligchars, carrot;
 		for(var c=0; c<carr.length; c++){
 			// for(var g=ligs.length-1; g>-1; g--){
 			for(var g=0; g<ligs.length; g++){
 				ligchars = hexToGlyph(ligs[g].id);
-				// debug('\t checking ' + ligchars);
+				debug('\t checking ' + ligchars);
 				carrot = carr.slice(c, (c+ligchars.length)).join('');
-				// debug('\t against ' + carrot);
+				debug('\t against ' + carrot);
 				if(carrot === ligchars){
 					carr.splice(c, ligchars.length, ligchars);
-					// debug('\t !Ligature Found! array['+c+'] is now ' + carr[c]);
+					debug('\t !Ligature Found! array['+c+'] is now ' + carr[c]);
 				}
 			}
 		}
 
-		// debug(' findAndMergeLigatures - END\n');
+		debug(' findAndMergeLigatures - END\n');
 		return carr;
 	}
 
 	function drawLine(y){
-		// debug('TESTDRIVE - Drawing h line at ' + y);
+		debug('TESTDRIVE - Drawing h line at ' + y);
 		y = y.makeCrisp();
 		_UI.testdrive.ctx.strokeStyle = _UI.colors.blue.l65;
 		_UI.testdrive.ctx.beginPath();
@@ -275,7 +252,7 @@
 	function createimg(){
 		var imgd = document.getElementById('tdcanvas').toDataURL();
 
-		var win = window.open(document.location.href, 'Glyphr Test Drive');
+		var win = window.open(document.location.href, 'Glyphr Studio Test Drive');
 
 		win.document.write('<!DOCTYPE html><html>'+
 		'<head><title>Glyphr - Test Drive Image</title></head>'+
@@ -284,4 +261,223 @@
 		'</html>');
 	}
 
-// end of file
+	function removeEmptyStringInputs(val) {
+		if(val === '""' || val === "''") return '';
+		else return trim(val);
+	}
+
+	function sortLigatures() {
+		var temp;
+		var sortarr = [];
+
+		for(var n in _GP.ligatures) { if(_GP.ligatures.hasOwnProperty(n)){
+			temp = _GP.ligatures[n];
+			sortarr.push({'id':n, 'ligature':temp});
+		}}
+
+		sortarr.sort(function(a,b){
+			if(a.id && b.id){
+				if(a.id.length === b.id.length){
+					if (a.id > b.id) return 1;
+					if (a.id < b.id) return -1;
+				} else {
+					return b.id.length - a.id.length;
+				}
+			} else return 0;
+		});
+
+		_GP.ligatures = {};
+
+		for(var s=0; s<sortarr.length; s++){
+			temp = sortarr[s];
+			_GP.ligatures[temp.id] = temp.ligature;
+		}
+
+		return sortarr;
+	}
+
+	function checkUI(varname, currbool, doredraw, invert){
+		//debug("CHECKUI -  varname:" + varname + " doredraw:" + doredraw);
+		var idname = varname.split('.');
+		idname = idname[idname.length-1];
+		if(invert) currbool = !currbool;
+
+		var re = '<input type="checkbox"';
+		re += (currbool? ' checked ' : ' ');
+		re += 'id="'+idname+'"';
+		re += 'onclick="'+varname+' = !'+varname+';';
+
+		if(doredraw){
+			re += ' history_put(\'Toggled '+idname+': '+!currbool+'\');';
+			re += ' redraw({calledby:\'checkbox '+idname+'\'});';
+		}
+
+		re += '"/>';
+
+		return re;
+	}
+
+
+//-------------------
+// View
+//-------------------
+	function setView(oa){
+
+		// Check for which to set
+		if(isval(oa.dx)){ _UI.view.dx = oa.dx; }
+		if(isval(oa.dy)){ _UI.view.dy = oa.dy; }
+		if(isval(oa.dz)){ _UI.view.dz = oa.dz; }
+
+		//debug('SETVIEW - passed ' + JSON.stringify(oa) + ' selectedglyph ' + _UI.selectedglyph + ' VIEWS is\n' + JSON.stringify(_UI.views));
+	}
+
+	function getView(calledby){
+		return clone(_UI.view);
+	}
+
+
+//	-----------------------------------------------
+//	Convert between Saved values and Canvas values
+//	-----------------------------------------------
+	//convert stored x-y coord to canvas x-y
+	function sx_cx(sx){
+		var v = getView('sx_cx');
+		var canvasx = v.dx;
+		canvasx += (sx*v.dz);
+		return canvasx || v.dx;
+	}
+
+	function sy_cy(sy){
+		var v = getView('sy_cy');
+		var canvasy = v.dy;
+		canvasy -= (sy*v.dz);
+		return canvasy || v.dy;
+	}
+
+	//convert canvas x-y inputs to saved shape x-y
+	function cx_sx(cx){
+		var v = getView('cx_sx');
+		return ((cx-v.dx)/(v.dz));
+	}
+
+	function cy_sy(cy){
+		var v = getView('cy_sy');
+		return ((v.dy-cy)/(v.dz));
+	}
+
+
+//	--------------------------
+//	Hex Conversion Functions
+//	--------------------------
+	function decToHex(d) { var dr = Number(d).toString(16); while(dr.length < 4) { dr = '0'+dr; } return '0x'+dr.toUpperCase(); }
+
+	function decToHTML(d) { return hexToHTML(decToHex(d)); }
+
+	function glyphToHex(s) {
+		var result = '';
+		for(var i=0; i<s.length; i++) result += decToHex(String(s).charCodeAt(i));
+		return result;
+	}
+
+	function glyphToHexArray(s) {
+		var result = [];
+		for(var i=0; i<s.length; i++) result.push(decToHex(String(s).charCodeAt(i)));
+		return result;
+	}
+
+	function hexToGlyph(u) {
+		if(String(u).charAt(1) !== 'x') u = String(decToHex(u));
+		// debug('\n hexToGlyph - START');
+		// debug('\t passed ' + u + ' which is a ' + typeof u);
+		u = u.split('0x');
+		var result = '';
+		for(var i=0; i<u.length; i++){ if(u[i] !== ''){
+			u[i] = String.fromCharCode('0x'+u[i]);
+			// debug('\t added ' + u[i]);
+			if(u[i]) result += u[i];
+		}}
+		// debug(' hexToHTML - END\n');
+		return result;
+	}
+
+	function hexToHTML(h) {
+		// debug('\n hexToHTML - START');
+		// debug('\t passed ' + h);
+		if(!h || h.indexOf('0x') < 0) return false;
+
+		h = String(h).split('0x');
+		var result = '';
+		for(var i=0; i<h.length; i++){ if(h[i] !== ''){
+			h[i] = ('0x'+h[i]);
+			h[i] = parseInt(h[i],16);
+			if(h[i]) result += ('&#'+h[i]+';');
+		}}
+		return result;
+	}
+
+	function hexToUnicodeHex(h){
+		return (h.replace(/0x/, '&#x') + ';');
+	}
+
+	function parseUnicodeInput(str) {
+		// takes any kind or number of input
+		// Unicode, Hex, or glyph
+		// and returns an array of padded hex values
+
+		// debug('\n parseUnicodeInput - START');
+		// debug('\t passed ' + str);
+
+		if(!str) return false;
+
+		var entries = [];
+		var results = [];
+
+		var prefix = str.substr(0,2);
+		if(isInputUnicode(str)) {
+			str = str.replace(/u\+/g, 'U+');
+			entries = str.split('U+');
+		} else if (isInputHex(str)) {
+			str = str.replace(/0X/g, '0x');
+			entries = str.split('0x');
+		} else {
+			return glyphToHexArray(str);
+		}
+
+		var te;
+		for(var e=0; e<entries.length; e++){
+			te = entries[e];
+			te = te.replace(/;/g, '');
+			if(te !== ''){
+				while(te.length < 4) te = '0'+te;
+				te = ('0x'+te.toUpperCase());
+				// debug('\t parsed ' + e + ' as ' + te);
+				results.push(te);
+			}
+		}
+
+		// debug('\t returning ' + JSON.stringify(results));
+		// debug('parseUnicodeInput - END\n');
+		return results;
+	}
+
+	function isInputUnicode(str) {
+		str = str.replace(/u\+/g, 'U+');
+		var count = 0;
+		var pos = str.indexOf('U+');
+		while(pos !== -1){
+			count ++;
+			pos = str.indexOf('U+', pos+2);
+		}
+		return count;
+	}
+
+	function isInputHex(str) {
+		str = str.replace(/0X/g, '0x');
+		var count = 0;
+		var pos = str.indexOf('0x');
+		while(pos !== -1){
+			count ++;
+			pos = str.indexOf('0x', pos+2);
+		}
+		return count;
+	}
---
+++ app/settings.js
@@ -0,0 +1,79 @@
+_GP = {};
+
+_UI = {
+	
+	// Version
+	'thisGlyphrStudioVersion': 'Version 0.1',
+	'thisGlyphrStudioVersionNum': '0.0.1',
+
+	'coremode': true,
+	'devmode': true,
+
+	'view': {},
+	'testdrive': {
+		'ctx': false,
+		'canvas': false,
+		'sampletext': '',
+		'fontscale': 100,
+		'fontsize': 100,
+		'linegap': false,
+		'padsize': 0,
+		'showglyphbox': false,
+		'showhorizontals': false,
+		'flattenglyphs': true,
+		'cache':{}
+	},
+
+	'colors': {
+
+		// ACCENT BLUE
+		'blue': {
+			'l95': 'rgb(225,245,255)',
+			'l85': 'rgb(155,221,255)',
+			'l75': 'rgb(80,196,255)',
+			'l65': 'rgb(0,170,255)',		// Primary Accent
+			'l55': 'rgb(0,140,210)',
+			'l45': 'rgb(0,113,170)',
+			'l35': 'rgb(0,90,135)',
+			'l25': 'rgb(0,63,95)',
+			'l15': 'rgb(0,43,65)',
+			'l05': 'rgb(0,20,30)'
+		},
+
+		// ACCENT GREEN
+		'green': {
+			'l95': 'rgb(185,255,226)',
+			'l85': 'rgb(0,245,144)',
+			'l75': 'rgb(0,210,123)',
+			'l65': 'rgb(0,180,105)',
+			'l55': 'rgb(0,150,88)',
+			'l45': 'rgb(0,125,73)',
+			'l35': 'rgb(0,95,55)',
+			'l25': 'rgb(0,70,41)',
+			'l15': 'rgb(0,45,26)',
+			'l05': 'rgb(0,20,11)'
+		},
+
+		// COOL GRAYSCALE
+		'gray':{
+			'offwhite':	'rgb(250,252,255)',	// Off White
+			'l90': 'rgb(229,234,239)',		// LIGHTER
+			'l80': 'rgb(204,209,214)',
+			'l70': 'rgb(178,183,188)',
+			'l60': 'rgb(153,158,163)',
+			'l65': 'rgb(153,158,163)',		// Fake for outline color
+			'l50': 'rgb(127,134,137)',
+			'l40': 'rgb(102,107,112)',
+			'l30': 'rgb(76,81,86)',
+			'l20': 'rgb(51,56,61)',
+			'l10': 'rgb(25,30,35)'			// DARKER
+		},
+
+		// RED FOR ERROR
+		'error': {
+			'light': 'rgb(240,210,215)',	// os red warning color light
+			'medium': 'rgb(240,15,54)',	// os red warning color
+			'dark': 'rgb(105,45,55)'	// os red warning color dark
+		}
+	}
+};
